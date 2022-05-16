46176deed7731c60b58e0637db01847cbcfe2c64
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2022-05-17 08:12:54 +1000

    Initial Commit

---
+++ ImageZStack/ImageZStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageZStackApp.swift
+//  ImageZStack
+//
+//  Created by Giuseppe Sapienza on 17/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageZStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
