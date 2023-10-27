ff451371f227187a2ea7d65a3549360398a96ea5
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2023-10-28 10:38:39 +1100

    Exercise

--- ImageZStack/ContentView.swift
+++ ImageZStack/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            profileImage
+            sectionTitleView
+            
+            ScrollView(showsIndicators: false) {
+                myCustomImage(etnaImageURL)
+                myCustomImage(melbourneImageURL)
+                myCustomImage(taorminaImageURL)
+                myCustomImage(sydneyImageURL)
+            }
+            
+        }.padding()
     }
+    
+    var profileImage: some View {
+        Image("peppe")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 100, height: 100)
+            .clipShape(Circle())
+    }
+    
+    var sectionTitleView: some View {
+        HStack {
+            Text("Pictures")
+                .font(.title2)
+                .padding(.top, 20)
+            
+            Spacer()
+        }
+    }
+    
+    func myCustomImage(_ url: URL) -> some View {
+        AsyncImage(url: url) { image in
+            image
+                .resizable()
+                .clipped()
+        } placeholder: {
+            ProgressView()
+        }
+        .frame(height: 200)
+        .frame(maxWidth: .infinity)
+        .background(Color.gray.opacity(0.3))
+        .cornerRadius(8)
+    }
+    
+    let etnaImageURL: URL = URL(string: "https://static.independent.co.uk/2021/02/25/16/APTOPIX_Italy_Etna_Volcano_Eruption_91254.jpg?quality=75&width=982&height=726&auto=webp")!
+    
+    let melbourneImageURL: URL = URL(string: "https://media.istockphoto.com/photos/melbourne-at-dusk-picture-id493621192?k=20&m=493621192&s=612x612&w=0&h=G2D4HuVz8TbTQIOiySadIL4ZYLQO36kf8kyjuDITWqU=")!
+    
+    let taorminaImageURL: URL = URL(string: "https://www.thethinkingtraveller.com/media/Resized/SICILY%20local%20areas/Taormina_and_beaches/1920/Taormina%20(1).jpg")!
+    
+    let sydneyImageURL: URL = URL(string: "https://a.cdn-hotels.com/gdcs/production0/d1645/0c67ff64-cf54-4886-91dd-89aa601463af.jpg")!
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
